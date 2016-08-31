60e9a50828a514734c2e0bf35cd6558702247565
Candy <296230508@qq.com>
2016-08-31 15:31:06 +0800

    第一章
    2016.08.31

---
+++ NetEnglish.java
@@ -0,0 +1,38 @@
+	/*
+	 *  Connectivity 连通性
+	 *	NetWork 网络
+	 *	Node 结点
+	 *  Link 链路
+	 *	Internet 因特网
+	 *	National Science Foundtion 美国国家科学基金会NSF
+	 *	Internet eXchange Point 因特网交换点IXP
+	 *  World Wide Web 万维网 WWW
+	 *  Internet Society 因特网协会
+	 *  Internet Architecture Board 因特网体系结构委员会
+	 *  end system 端系统
+	 *	client 客户
+	 *  server 服务
+	 *	peer to peer P2P 对等连接
+	 *  router 路由器
+	 *  packet switching 分组交换
+	 *  circuit switching 电路交换
+	 *  message 报文
+	 *  protocol 协议
+	 *  overhead 开销
+	 *  Wide Area Network 广域网
+	 *  Metropolitan Area Network 城域网
+	 *  Local Area Network 局域网
+	 *  Personal Area Network 个人局域网
+	 *  public network 公用网
+	 *  private network 专用网
+	 *  bit 比特
+	 *  data rate 数据率
+	 *  bandwidth 带宽
+	 *  throughput 吞吐量
+	 *  delay 延时
+	 *  transmission delay 发送延时
+	 *  propagation delay 转播延时
+	 *  activate 激活
+	 *  Open Systems Interconnetion Reference Model 开发系统互连基本参考模型OSI
+	 */
+	
